efd689e66770cd91a68e7c7e64abf607630a7218
geyerje <geyerje@rose-hulman.edu>
2018-05-24 13:39:15 -0400

    .

--- src/problem3.py
+++ src/problem3.py
@@ -4,7 +4,7 @@ Final exam, problem 3.
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 import time
@@ -58,6 +58,35 @@ def run_test_problem3():
 
 
 def problem3(point, circle1, circle2, window):
+    line1 = rg.Line(point, circle1.center)
+    line2 = rg.Line(circle1.center, circle2.center)
+    line3 = rg.Line(circle2.center, point)
+    line1.color = circle1.fill_color
+    line2.color = circle1.fill_color
+    line3.color = circle1.fill_color
+
+    line4 = rg.Line(line1.get_midpoint(), line2.get_midpoint())
+    line5 = rg.Line(line2.get_midpoint(), line3.get_midpoint())
+    line6 = rg.Line(line3.get_midpoint(), line1.get_midpoint())
+    line4.color = circle2.fill_color
+    line5.color = circle2.fill_color
+    line6.color = circle2.fill_color
+
+
+    circle1.attach_to(window)
+    circle2.attach_to(window)
+    point.attach_to(window)
+
+    line1.attach_to(window)
+    line2.attach_to(window)
+    line3.attach_to(window)
+    line4.attach_to(window)
+    line5.attach_to(window)
+    line6.attach_to(window)
+
+    window.render()
+
+
     """
     See   problem3_picture.pdf   in this project for pictures
     that may help you better understand the following specification:
@@ -93,7 +122,7 @@ def problem3(point, circle1, circle2, window):
       :type window:    rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
 
