7e4939f3a9ad8f7a4a9b23a9348fa6bcf7ab0afc
geyerje <geyerje@rose-hulman.edu>
2018-05-24 13:13:17 -0400

    .

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and James (Bo) Geyer.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
 
 
@@ -73,6 +73,8 @@ def run_test_problem1a():
 
 
 def problem1a(numbers):
+    tot = numbers[0] + numbers[len(numbers)//2] + numbers[len(numbers) - 1]
+    return tot
     """
     What comes in:
       -- A sequence of numbers, with the sequence having a length
@@ -90,7 +92,7 @@ def problem1a(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
